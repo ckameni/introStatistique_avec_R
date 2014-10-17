4518fa9f106da7821fd056de7c77d232c4404eef
ckameni <e.c.kameni@aim.com>
2014-10-17 19:41:31 +0200

    lab 1 completed!

--- lab01.R
+++ lab01.R
@@ -75,9 +75,9 @@ table(smp$abus)
 sum(table(smp$abus))
 
 
+
 ## ------------------------------------------------------------------------
 table(smp$abus, useNA="always")
-
 
 ## ------------------------------------------------------------------------
 sum(is.na(smp$abus))
@@ -88,6 +88,8 @@ sum(complete.cases(smp$abus))
 ## ------------------------------------------------------------------------
 head(factor(smp$abus))
 head(factor(smp$abus, labels=c("Non", "Oui")))
+
+
 
 
 ## ------------------------------------------------------------------------
@@ -103,6 +105,7 @@ table(smp$n.enfant, useNA="always")
 
 head(smp$n.enfant > 4)
 table(smp$n.enfant > 4)
+
 
 
 ## ------------------------------------------------------------------------
@@ -124,3 +127,60 @@ table(smp$age.cat)
 ## ------------------------------------------------------------------------
 save(smp, file="smp_v1.rda")
 dir(pattern="csv")
+
+
+
+################################################################################
+##################################     Quiz 1   ################################
+################################################################################
+
+smp <- read.csv2("smp2.csv")
+
+#4
+z<-sample(0:1,10,1)
+z
+
+da1<-factor(z, labels=c("Non", "Oui"));da1
+
+das<-factor(z, levels=c(1,0),labels=c("Non", "Oui"),na.rm=T),das
+#8
+
+
+yy<-sample(1:9,10,1);yy
+yy<-factor(yy>=5,labels=c("<5", "5+"));yy;class(yy)
+
+   sa<-factor(n.fratrie>=5,labels=c("<5", "5+")) 
+ t
+       factor(table(n.fratrie>=5),labels=c("<5", "5+"))
+      names(smp)
+
+class(age)
+x<-head(age);x
+class(y)
+
+#9
+
+
+length(subset(smp,(age>=20 & cage<=30))$age)
+
+#10
+
+mean(dur.interv[1:300],na.rm=T)
+median(dur.interv[1:300],na.rm=T)
+
+
+#6e
+
+
+attach(smp)
+min(smp$age,na.rm=T)
+summary(age)
+
+table(cut(age,breaks=quantile(age,na.rm=T)  ) )
+
+#7
+IQR(dur.interv,na.rm=T)
+
+summary(dur.interv)
+
+table(cut(dur.interv,breaks=quantile( dur.interv,na.rm=T),right=T,include.lowest=T  ) )
